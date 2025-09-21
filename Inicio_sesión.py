e74852d8eb67ed01e0aad6a461693bad140848bc
Chompire <benjasave1997@gmail.com>
2025-09-21 04:06:42 -0300

    main

---
+++ Inicio_sesión.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Mi primera app con Flet"
+    page.vertical_alignment = "center"
+
+    txt_nombre = ft.TextField(label="Escribe tu nombre")
+    saludo = ft.Text()
+
+    def saludar(e):
+        saludo.value = f"¡Hola, {txt_nombre.value}!"
+        page.update()
+
+    page.add(
+        txt_nombre,
+        ft.ElevatedButton("Saludar", on_click=saludar),
+        saludo
+    )
+
+# Ejecutar en el navegador
+ft.app(target=main, view=ft.WEB_BROWSER)
