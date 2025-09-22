ecabc11a0ff625b065cd5f5ba96eba251763dd0f
Chompire <benjasave1997@gmail.com>
2025-09-22 16:15:00 -0300

    main

--- Interfaz_profesor.py
+++ Interfaz_profesor.py
@@ -1,23 +1,37 @@
 # menu.py
 import flet as ft
-
 def mostrar_menu_principal(page: ft.Page, nombre_usuario):
     page.clean()
 
+    def buttonclick(e):
+        registro_alumno(page)
+        page.update()
     page.add(
-        ft.Column(
+        ft.Container(
+            ft.Column(
             [
                 ft.Text(f"Bienvenido, {nombre_usuario}!", size=25, weight="bold", color="green"),
                 ft.Text("Esta es la segunda interfaz 🎉", size=18),
-                ft.ElevatedButton("Nuevo test", on_click= registro_alumno(page))
+                ft.ElevatedButton("Nuevo test", on_click=buttonclick)
             ],
             alignment="center",
             horizontal_alignment="center",
             expand=True
+            )
         )
+        
     )
 def registro_alumno(page:ft.Page):
     page.clean()
+    def back(e):
+        mostrar_menu_principal(page)
+        page.update()
     page.add(
-        ft.Text("Ingresa los datos del alumno.")
+        ft.Container(
+            ft.Column(
+                [ft.Text("Ingresa los datos del alumno."), ft.ElevatedButton("Volver", on_click= back)]
+            )
+            
+        )
+        
     )
