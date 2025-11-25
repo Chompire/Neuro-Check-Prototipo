80fe9f7066ed4a71a1b2f7f935d87015a1ad079b
benjaminsaavedra-sketch <benjamin.saavedra@northamerican.cl>
2025-11-25 15:24:27 -0300

    main

--- src/db.py
+++ src/db.py
@@ -1,7 +1,7 @@
 import pyodbc
 CONNECTION_STRING = (
     'Driver={ODBC Driver 17 for SQL Server};'
-    r'Server=CHOMPIRE\SQLEXPRESS;'
+    r'Server=UBNTCONTROLLER\SQLEXPRESS;'
     'Database=NeuroCheckDB;'
     'Trusted_Connection=yes;'
 )
